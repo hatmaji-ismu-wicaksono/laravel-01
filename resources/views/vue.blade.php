ed9fd6c4653b64f3596ba43a03585ffefea147f2
hatmaji_ismu <hatmaji.ismu@gmail.com>
2023-02-24 09:25:46 +0700

    #002

--- resources/views/vue.blade.php
+++ resources/views/vue.blade.php
@@ -1,7 +1,7 @@
 <?php
-    $dto = [
-        'title' => 'Hello World',
-    ];
+$dto = [
+    'title' => 'Hello World',
+];
 ?>
 
 <!DOCTYPE html>
@@ -11,12 +11,19 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
     <title>Document</title>
 </head>
 
 <body>
     <div id="app">
-        <example-component :param='@json($dto)'></example-component>
+        <v-app>
+            <v-main>
+                {{-- <topnav></topnav> --}}
+                <example-component :param='@json($dto)'></example-component>
+                <vuetify-sandbox></vuetify-sandbox>
+            </v-main>
+        </v-app>
     </div>
     <script src="{{ asset('js/app.js') }}"></script>
 </body>
